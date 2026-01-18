22d2cc78eef4c7ccfc33e192bf0be2afa89d2ea9
Dmytro_L <dimka4545452@gmail.com>
2026-01-18 16:36:18 +0100

    corrected the errors

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -6,15 +6,21 @@ import { fetchNoteById } from '@/lib/api';
 import css from "./NoteDetails.module.css"
 
 
+
 export default function NoteDetailsClient() {
     
     const params = useParams();
     const id = params.id as string;
     
-    const { data: note,  error } = useQuery({
+    const { data: note,  error, isLoading } = useQuery({
         queryKey: ['note', id],
         queryFn: () => fetchNoteById(id),
+        refetchOnMount: false
     });
+
+    if(isLoading){
+        return <p className={css.status}>Loading, please wait</p>
+    }
     
     
     if (error || !note) return <p className={css.status}>Something went wrong.</p>;
